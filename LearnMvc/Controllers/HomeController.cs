ff6686835a5c545db1da3be11350b4e0581de0fc
TOMSUN <readyourlove@163.com>
2019-06-21 17:33:31 +0800

    1111

---
+++ LearnMvc/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LearnMvc.Models;
+
+namespace LearnMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+//            return "start";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult Ask()
+        {
+//            return "";
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Ask(Student student)
+        {
+            if (ModelState.IsValid)
+                return View("AskSuccess", student);
+            return View();
+        }
+    }
+}
---
+++ LearnMvc/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LearnMvc.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "姓名不能为空")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "姓名不能为空")]
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage = "请输入有效的Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "电话号码不能为空")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "请选择是否到场")]
+        public bool? WillAtend { get; set; }
+    }
+}
